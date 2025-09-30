4901c7b3221a1accdf6be6786ceb6548111bc067
Kirill <masteryoda01010101@gmail.com>
2025-09-30 11:31:32 +0300

    new status for task for user

--- service/internal/dto/structures.go
+++ service/internal/dto/structures.go
@@ -2,6 +2,14 @@ package dto
 
 type TaskStatus string
 
+// todo maybe new status
+const (
+	StatusPending TaskStatus = "pending"
+	StatusRunning TaskStatus = "in_progress"
+	StatusDone    TaskStatus = "done"
+	StatusFailed  TaskStatus = "failed"
+)
+
 type Task struct {
 	ID     string     `json:"id"`
 	URLs   []string   `json:"urls"`
