3c51257d250bdc9c6684cae16f68b7686a0637b2
Kirill <masteryoda01010101@gmail.com>
2025-09-30 13:32:26 +0300

    updated structurse

--- service/internal/dto/structures.go
+++ service/internal/dto/structures.go
@@ -17,7 +17,9 @@ type Task struct {
 	URLs      []string   `json:"urls"`
 	Status    TaskStatus `json:"status"`
 	Total     int        `json:"total"`
+	Completed int        `json:"completed"`
 	CreatedAt time.Time  `json:"created_at"`
+	Error     string     `json:"error"`
 }
 
 type TaskImpl interface { // todo dependency for interface for testing
