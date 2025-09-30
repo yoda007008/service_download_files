8715f00f97a35bae3d1dd4e21b4127881e77518f
Kirill <masteryoda01010101@gmail.com>
2025-09-30 17:18:44 +0300

    update help functions

--- service/internal/handlers/helpers.go
+++ service/internal/handlers/helpers.go
@@ -3,7 +3,17 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"testtask/service/internal/service"
 )
+
+func NewTaskHandlers(manager *service.TaskManager) *TaskHandlers {
+	return &TaskHandlers{manager: *manager}
+}
+
+func (h *TaskHandlers) RegisterRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/tasks", h.CreateTask)
+
+}
 
 func writeJSON(w http.ResponseWriter, data any, code int) {
 	w.Header().Set("Content-Type", "application/json")
